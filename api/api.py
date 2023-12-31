3a2104a2888656b65e7f0f42bb2f83277152c691
amoooooo <amor.budiyanto2006@gmail.com>
2023-12-31 14:32:29 +1300

    comments & docstrings

--- api/api.py
+++ api/api.py
@@ -10,10 +10,13 @@ import src.server as auth
 import sys
 import argparse
 
+# set up flask API
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY", os.urandom(24))
+# enables CORS
 CORS(app)
 
+# use argeparse for file logs
 parser = argparse.ArgumentParser()
 parser.add_argument(
     "--logfile",
@@ -31,7 +34,7 @@ if args.logfile:
     def close_log_file(error):
         log_file.close()
 
-
+# load valid API keys
 API_KEYS_DIR = Path(Path(__file__).parent, "src", "valid_api_keys.json")
 with open(API_KEYS_DIR, "r") as f:
     VALID_KEYS = json.loads(f.read())
@@ -45,6 +48,7 @@ talisman = Talisman(
 
 
 def api_key_auth():
+    """Authenticate User"""
     api_key = request.headers.get("X-API-Key")
     if not api_key or api_key not in VALID_KEYS:
         return jsonify({"error": "Unauthorized"}), 401
@@ -52,23 +56,31 @@ def api_key_auth():
 
 @app.route("/signup", methods=["POST"])
 def register_client():
+    """User account sign up"""
     data = request.get_json()
+
+    # check for all params
     expected = ["name", "password", "team"]
     if not (list(data) == expected):
-        return jsonify({"error": "Invalid request, missing expected parameter"}), 400
+        return jsonify({"error":
+                        "Invalid request, missing expected parameter"}), 400
 
+    # check team selected is valid
     teams = ["ngata", "rutherford", "britten", "blake", "cooper", "sheppard"]
     if data["team"].lower() in teams:
         team = {"team": data["team"].lower()}
     else:
         return jsonify({"error": "Invalid request, unexpected team"}), 400
 
+    # generate random API Key
     api_key = str(uuid.uuid4())
-    new_client = data | {"api_key": api_key, "active_quizzes": {}, "points": 0} | team
+    new_client = data | {"api_key": api_key,
+                         "active_quizzes": {}, "points": 0} | team
     message, access = auth.signUp(new_client)
     if not access:
         return jsonify({"error": message}), 400
 
+    # save API key and user
     with open(API_KEYS_DIR, "r+") as f:
         route = json.loads(f.read()) | {api_key: data["name"]}
         f.seek(0)
@@ -82,16 +94,22 @@ def register_client():
 
 @app.route("/login", methods=["POST"])
 def login_client():
+    """Log in client"""
     data = request.get_json()
+
+    # check expected params
     expected = ["name", "password"]
     if not (list(data) == expected):
-        return jsonify({"error": "Invalid request, missing expected parameter"}), 400
+        return jsonify({"error":
+                        "Invalid request, missing expected parameter"}), 400
 
+    # check password is correct
     message, access = auth.login(data["name"], data["password"])
     if not access:
         print(message)
         return jsonify({"error": message}), 400
 
+    # if given access, load user info
     with open(Path(auth.PROFILES_DIR, data["name"] + ".json"), "r") as f:
         api_key = json.loads(f.read())["api_key"]
     return jsonify({"api_key": api_key}), 201
@@ -99,36 +117,19 @@ def login_client():
 
 @app.route("/get_score", methods=["GET"])
 def get_score():
+    """Give score"""
     api_key_auth()
     with open(
-        Path(auth.PROFILES_DIR, VALID_KEYS[request.headers.get("X-API-Key")] + ".json"),
+        Path(auth.PROFILES_DIR,
+             VALID_KEYS[request.headers.get("X-API-Key")] + ".json"),
         "r",
     ) as f:
         return jsonify({"score": json.loads(f.read())["points"]}), 201
 
 
-@app.route("/render_infinite_quiz", methods=["GET"])
-def render_quiz():
-    api_key_auth()
-    data = request.get_json()
-    expected = ["quiz_name"]
-    if not (list(data) == expected):
-        return jsonify({"error": "Invalid request, missing expected parameter"}), 400
-
-    user = VALID_KEYS[request.headers.get("X-API-Key")]
-    with open(Path(auth.PROFILES_DIR, user + ".json"), "r") as f:
-        route = json.loads(f.read())
-        question = route[data["quiz_name"]]["results"][route["index"]]
-
-        options = question["incorrect_answers"] + question["correct_answer"]
-        del question["incorrect_answers"]
-        del question["correct_answers"]
-
-    return jsonify(data=question | options), 200
-
-
 @app.route("/render_leaderboard", methods=["GET"])
 def render_leaderboard():
+    """Get Leaderboard"""
     api_key_auth()
     with open(Path(auth.DEFAULT_DIR, "teams.json"), "r") as f:
         leaderboard = json.loads(f.read())
@@ -137,18 +138,23 @@ def render_leaderboard():
 
 @app.route("/start_infinite_quiz", methods=["POST", "OPTIONS"])
 def start_infinite_quiz():
+    """Start infinite quiz session"""
     if request.method == "OPTIONS":
         response = jsonify({"status": 200})
         response.headers.add("Allow", "POST")
-        response.headers.add("Access-Control-Allow-Headers", "Content-Type, X-API-Key")
+        response.headers.add("Access-Control-Allow-Headers",
+                             "Content-Type, X-API-Key")
         response.headers.add("Access-Control-Allow-Methods", "POST")
         return response
 
     api_key_auth()
     data = request.get_json()
+
+    # expected params
     expected = ["questions", "lives"]
     if not (list(data) == expected):
-        return jsonify({"error": "Invalid request, missing expected parameter"}), 400
+        return jsonify({"error":
+                        "Invalid request, missing expected parameter"}), 400
 
     global infinite_sessions
     infinite_sessions[request.headers.get("X-API-Key")] = data
@@ -157,10 +163,12 @@ def start_infinite_quiz():
 
 @app.route("/expand_infinite_quiz", methods=["POST", "OPTIONS"])
 def expand_infinite_quiz():
+    """Expand the user's infinite quiz session"""
     if request.method == "OPTIONS":
         response = jsonify({"status": "success"})
         response.headers.add("Allow", "POST")
-        response.headers.add("Access-Control-Allow-Headers", "Content-Type, X-API-Key")
+        response.headers.add("Access-Control-Allow-Headers",
+                             "Content-Type, X-API-Key")
         response.headers.add("Access-Control-Allow-Methods", "POST")
         return response
 
@@ -168,19 +176,24 @@ def expand_infinite_quiz():
     data = request.get_json()
     expected = ["questions"]
     if not (list(data) == expected):
-        return jsonify({"error": "Invalid request, missing expected parameter"}), 400
+        return jsonify({"error":
+                        "Invalid request, missing expected parameter"}), 400
 
     global infinite_sessions
-    infinite_sessions[request.headers.get("X-API-Key")]["questions"].append(data)
+    api_key = request.headers.get("X-API-Key")
+    # append the live session
+    infinite_sessions[api_key]["questions"].append(data)
     return jsonify({"status": "Quiz Appended"}), 200
 
 
 @app.route("/answer_infinite_quiz", methods=["POST", "OPTIONS"])
 def answer_infinite_quiz():
+    """Answer an infinite gamemode question"""
     if request.method == "OPTIONS":
         response = jsonify({"status": "success"})
         response.headers.add("Allow", "POST")
-        response.headers.add("Access-Control-Allow-Headers", "Content-Type, X-API-Key")
+        response.headers.add("Access-Control-Allow-Headers",
+                             "Content-Type, X-API-Key")
         response.headers.add("Access-Control-Allow-Methods", "POST")
         return response
 
@@ -188,16 +201,20 @@ def answer_infinite_quiz():
     data = request.get_json()
     expected = ["selected"]
     if not (list(data) == expected):
-        return jsonify({"error": "Invalid request, missing expected parameter"}), 400
+        return jsonify({"error":
+                        "Invalid request, missing expected parameter"}), 400
 
     api_key = request.headers.get("X-API-Key")
     global infinite_sessions
     question = infinite_sessions[api_key]["questions"][0]
     user = VALID_KEYS[api_key]
 
+    # delete question
     del infinite_sessions[api_key]["questions"][0]
 
+    # check if correct
     if question["correct_answer"] == data["selected"]:
+        # if correct add score to user and their house team
         with open(Path(auth.PROFILES_DIR, user + ".json"), "r+") as teams:
             user_info = json.loads(teams.read())
             user_info["points"] += 1
@@ -224,10 +241,12 @@ def answer_infinite_quiz():
             200,
         )
 
+    # if question is wrong, lose 1 life
     infinite_sessions[api_key]["lives"] -= 1
     return (
         jsonify(
-            {"status": "Wrong Answer", "lives": infinite_sessions[api_key]["lives"]}
+            {"status": "Wrong Answer",
+             "lives": infinite_sessions[api_key]["lives"]}
         ),
         200,
     )
@@ -235,10 +254,12 @@ def answer_infinite_quiz():
 
 @app.route("/start_quiz", methods=["POST", "OPTIONS"])
 def start_quiz():
+    """Start traditional quiz"""
     if request.method == "OPTIONS":
         response = jsonify({"status": 200})
         response.headers.add("Allow", "POST")
-        response.headers.add("Access-Control-Allow-Headers", "Content-Type, X-API-Key")
+        response.headers.add("Access-Control-Allow-Headers",
+                             "Content-Type, X-API-Key")
         response.headers.add("Access-Control-Allow-Methods", "POST")
         return response
 
@@ -246,12 +267,15 @@ def start_quiz():
     data = request.get_json()
     expected = ["quiz_name", "quiz"]
     if not (list(data) == expected):
-        return jsonify({"error": "Invalid request, missing expected parameter"}), 400
+        return jsonify({"error":
+                        "Invalid request, missing expected parameter"}), 400
 
+    # save quiz session to a file
     user = VALID_KEYS[request.headers.get("X-API-Key")]
     with open(Path(auth.PROFILES_DIR, user + ".json"), "r+") as f:
         route = json.loads(f.read())
-        route["active_quizzes"][data["quiz_name"]] = data["quiz"] | {"index": 0}
+        route["active_quizzes"][data["quiz_name"]] = (data["quiz"]
+                                                      | {"index": 0})
         if len(route["active_quizzes"]) > 5:
             del route["active_quizzes"][0]
 
@@ -264,10 +288,12 @@ def start_quiz():
 
 @app.route("/answer_quiz", methods=["POST", "OPTIONS"])
 def answer_quiz():
+    """Answer quiz in traditional game mode"""
     if request.method == "OPTIONS":
         response = jsonify({"status": "success"})
         response.headers.add("Allow", "POST")
-        response.headers.add("Access-Control-Allow-Headers", "Content-Type, X-API-Key")
+        response.headers.add("Access-Control-Allow-Headers",
+                             "Content-Type, X-API-Key")
         response.headers.add("Access-Control-Allow-Methods", "POST")
         return response
 
@@ -275,16 +301,19 @@ def answer_quiz():
     data = request.get_json()
     expected = ["quiz_name", "selected"]
     if not (list(data) == expected):
-        return jsonify({"error": "Invalid request, missing expected parameter"}), 400
+        return jsonify({"error":
+                        "Invalid request, missing expected parameter"}), 400
 
+    # open up quiz
     user = VALID_KEYS[request.headers.get("X-API-Key")]
     with open(Path(auth.PROFILES_DIR, user + ".json"), "r+") as f:
         route = json.loads(f.read())
         question = route["active_quizzes"][data["quiz_name"]]
 
-        print(question["results"][question["index"]]["correct_answer"])
-        print(data["selected"])
-        if question["results"][question["index"]]["correct_answer"] == data["selected"]:
+        # check if correct
+        correct = question["results"][question["index"]]["correct_answer"]
+        if (correct == data["selected"]):
+            # if correct add a point to user and their house team
             route["points"] += 1
             with open(Path(auth.DEFAULT_DIR, "teams.json"), "r+") as teams:
                 team = json.loads(teams.read())
@@ -306,8 +335,10 @@ def answer_quiz():
         f.seek(0)
         f.write(json.dumps(route))
         f.truncate()
+
+    # otherwise return wrong answer
     return jsonify({"status": "Wrong Answer"}), 200
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
--- api/src/server.py
+++ api/src/server.py
@@ -8,14 +8,17 @@ PROFILES_DIR = Path(DEFAULT_DIR, "profiles")
 
 
 def signUp(client: dict) -> (str, True | False):
+    """Sign up user online"""
     if os.path.exists(Path(PROFILES_DIR, str(client["name"]) + ".json")):
         return "User already exists", False
 
     client["password"] = generate_password_hash(client["password"])
 
+    # add user profile on the server
     with open(Path(PROFILES_DIR, client["name"] + ".json"), "w") as f:
         f.write(json.dumps(client))
 
+    # append their valid key to other api keys
     with open(Path(DEFAULT_DIR, "valid_api_keys.json"), "r+") as f:
         keys = json.loads(f.read()) | {client["api_key"]: client["name"]}
 
@@ -27,11 +30,14 @@ def signUp(client: dict) -> (str, True | False):
 
 
 def login(name: str, password: str) -> (str, str | None):
+    """Log in user"""
+    # check if user exist
     if os.path.exists(Path(PROFILES_DIR, str(name) + ".json")):
         with open(Path(PROFILES_DIR, str(name) + ".json"), "r") as info:
             profile = json.loads(info.read())
             correct = check_password_hash(profile["password"], password)
 
+            # check if password correct
             if correct:
                 return "Succesfully logged in", profile["api_key"]
             else:
